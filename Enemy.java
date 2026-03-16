8f0789a8da21251ba36ce2911007a6d22eee464d
miles <gaviganmiles@gmail.com>
2026-03-16 15:25:49 -0700

    made player and enemy

---
+++ Enemy.java
@@ -0,0 +1,10 @@
+public class Enemy extends GameObject {
+
+    public Enemy(double x, double y, int width, int height, String imagePath) {
+        super(x, y, width, height, imagePath);
+       
+    }
+
+    
+    
+}
